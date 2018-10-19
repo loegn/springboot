64ad47b96e5400dead4fd55213db6ee5174ad42d
liangenmao <liangenmao@yuantutech.com>
2018-10-19 11:18:25 +0800

    PropUtils

--- src/main/java/com/test/springboot/controller/IndexController.java
+++ src/main/java/com/test/springboot/controller/IndexController.java
@@ -1,12 +1,11 @@
 package com.test.springboot.controller;
 
-import com.test.springboot.pojo.User;
 import com.test.springboot.service.UserService;
-import com.test.springboot.utils.PropertiesUtils;
-import com.test.springboot.utils.BeanUtils;
+import com.test.springboot.utils.PropUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +20,11 @@ public class IndexController {
 
     public static final Logger log = LoggerFactory.getLogger(IndexController.class);
     @RequestMapping("/index")
-    public User index(HttpServletRequest request, HttpServletResponse response){
+    public Object index(HttpServletRequest request, HttpServletResponse response){
         request.getParameter("");
-        BeanUtils.getBean(PropertiesUtils.class).getLoggerFile();
-        BeanUtils.getBean(PropertiesUtils.class).getRoot();
-        BeanUtils.getBean(PropertiesUtils.class).getArray();
+        PropUtils.get("logging.level.root");
+        PropUtils.get("test.array[0].host");
+        PropUtils.get("test.c.a");
         return userService.getById(1);
     }
 }
