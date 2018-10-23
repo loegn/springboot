76219b219228670e41ed151977e8b0450cf6ebf6
liangenmao <liangenmao@yuantutech.com>
2018-10-23 15:55:32 +0800

    切换到jpa

--- src/main/java/com/test/springboot/entity/Permission.java
+++ src/main/java/com/test/springboot/entity/Permission.java
@@ -2,10 +2,17 @@ package com.test.springboot.entity;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
+@Entity
 public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private Integer version;
