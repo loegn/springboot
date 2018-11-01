824cb926987ea50e3f1ef794cd9d99856df44063
liangenmao <liangenmao@yuantutech.com>
2018-11-01 10:59:53 +0800

    项目部署到tomcat，修改一些配置

--- src/main/java/com/test/springboot/SpringbootApplication.java
+++ src/main/java/com/test/springboot/SpringbootApplication.java
@@ -3,9 +3,12 @@ package com.test.springboot;
 import com.test.springboot.config.CommonConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.WebApplicationInitializer;
 
 //import org.mybatis.spring.annotation.MapperScan;
 
@@ -14,7 +17,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ServletComponentScan(value = {CommonConstants.FILTER_SCAN, CommonConstants.LISTENER_SCAN})
 //@MapperScan(CommonConstants.MAPPERSCAN)
-public class SpringbootApplication {
+public class SpringbootApplication  extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(SpringbootApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
